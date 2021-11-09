2006d462908b35cfb821924ec1d74aefbd359ef9
Gilles <gilles.clarisse@student.howest.be>
2021-11-09 16:21:32 +0100

    Fix Nvidia Docker GPG key

--- install-deb.sh
+++ install-deb.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# Install script for Debian/Ubuntu only!
-
-# Store distribution
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
+# Install script for Debian/Ubuntu
 
 # Install curl
 apt install -y curl
@@ -16,11 +13,13 @@ curl -sL "https://github.com/docker/compose/releases/download/1.29.2/docker-comp
 chmod +x /usr/local/bin/docker-compose
 
 # Setup the CUDA network repository
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
 curl -fsSL https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin -o /etc/apt/preferences.d/cuda-repository-pin-600
 apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
 echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | tee /etc/apt/sources.list.d/cuda.list
 
 # Setup the Nvidia Docker repository
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 curl -fsSL https://nvidia.github.io/nvidia-docker/gpgkey | apt-key add -
 curl -fsSL https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | tee /etc/apt/sources.list.d/nvidia-docker.list
 
